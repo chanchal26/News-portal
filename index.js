7e27b43264265a2609872bd8954042674c2ed219
chanchal26 <chanchal.mch2613@gmail.com>
2022-09-03 14:20:46 +0600

    modal section done

--- index.js
+++ index.js
@@ -8,15 +8,16 @@ const displayNews = (catagories) => {
     const mainDiv = document.getElementById('news');
     newDiv = document.createElement('div');
     mainDiv.innerHTML = `
-                        <li class="list-group-item">Home</li>
-                        <li class="list-group-item">${catagories.news_category[0].category_name}</li>
-                        <li class="list-group-item">${catagories.news_category[1].category_name}</li>
-                        <li class="list-group-item">${catagories.news_category[2].category_name}</li>
-                        <li class="list-group-item">${catagories.news_category[3].category_name}</li>
-                        <li class="list-group-item text-primary bg-primary bg-opacity-10 px-2">${catagories.news_category[4].category_name}</li>
-                        <li class="list-group-item">${catagories.news_category[5].category_name}</li>
-                        <li class="list-group-item">${catagories.news_category[6].category_name}</li>
-                        <li class="list-group-item">${catagories.news_category[7].category_name}</li>
+                        
+            <a type="button" class="btn btn-light"><li class="list-group-item">Home</li></a>
+            <a type="button" class="btn btn-light"><li class="list-group-item">${catagories.news_category[0].category_name}</li></a>
+            <a type="button" class="btn btn-light"><li class="list-group-item">${catagories.news_category[1].category_name}</li></a>
+            <a type="button" class="btn btn-light"><li class="list-group-item">${catagories.news_category[2].category_name}</li></a>
+            <a type="button" class="btn btn-light"><li class="list-group-item">${catagories.news_category[3].category_name}</li></a>
+            <a type="button" class="btn btn-light"><li class="list-group-item text-primary bg-primary bg-opacity-10 px-2">${catagories.news_category[4].category_name}</li></a>
+            <a type="button" class="btn btn-light"><li class="list-group-item">${catagories.news_category[5].category_name}</li></a>
+            <a type="button" class="btn btn-light"><li class="list-group-item">${catagories.news_category[6].category_name}</li></a>
+            <a type="button" class="btn btn-light"><li class="list-group-item">${catagories.news_category[7].category_name}</li></a>         
         `;
 
 };
@@ -74,8 +75,30 @@ const displayDetails = (details) => {
     <i class="fa-regular fa-star"></i>
     <i class="fa-regular fa-star"></i>
     </div>
-    <div class="text-primary">
-    <i class="fa-solid fa-arrow-right"></i>
+
+    <div>
+    <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Show Details
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
     </div>
 
 
